3af91e9d842afe40252dd26de31272ed2b98922c
Ab-nin <129009184+Ab-nin@users.noreply.github.com>
2024-06-11 18:19:37 -0300

    Add files via upload

--- teste.py
+++ teste.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Teste')
 
 st.write("teste")
+
+with st.container():
+    dados = pd.read_excel('dados_CN.xlsx')
+    st.write(dados.head())
